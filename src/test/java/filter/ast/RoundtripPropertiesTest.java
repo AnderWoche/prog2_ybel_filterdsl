e8fcb07f15fb3a841fe1ed29ec33a7b1650f340d
Moldiy <36229419+AnderWoche@users.noreply.github.com>
2026-06-19 16:43:19 +0200

    added PropertieTests

--- src/test/java/filter/ast/RoundtripPropertiesTest.java
+++ src/test/java/filter/ast/RoundtripPropertiesTest.java
@@ -1,18 +1,101 @@
 package filter.ast;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import filter.ast.builder.AstBuilderPattern;
 import filter.ast.builder.AstBuilderVisitor;
 import filter.ast.builder.AstBuilders;
+import filter.ast.eval.Evaluator;
+import filter.ast.nodes.Expr;
 import filter.ast.printer.AstPrinter;
+import filter.model.Genre;
+import filter.model.MediaItem;
 import net.jqwik.api.*;
 
 public class RoundtripPropertiesTest {
 
-  // TODO
+  //HELPER
+  private static Expr viaVisitor(String query) {
+    return new AstBuilderVisitor().translate(AstBuilders.parse(query));
+  }
+
+  private static Expr viaPattern(String query) {
+    return new AstBuilderPattern().translate(AstBuilders.parse(query));
+  }
+
+  // TESTS
+  @Property
+  boolean roundtripVisitor(@ForAll("simpleQueries") String query) {
+    Expr ast = viaVisitor(query);
+    Expr reparsed = viaVisitor(AstPrinter.toString(ast));
+    return ast.equals(reparsed);
+  }
+
+  @Property
+  boolean roundtripPattern(@ForAll("simpleQueries") String query) {
+    Expr ast = viaPattern(query);
+    Expr reparsed = viaPattern(AstPrinter.toString(ast));
+    return ast.equals(reparsed);
+  }
+
+  @Property
+  boolean buildersAgree(@ForAll("simpleQueries") String query) {
+    return viaVisitor(query).equals(viaPattern(query));
+  }
+
+  @Property
+  boolean crossRoundtrip(@ForAll("simpleQueries") String query) {
+    Expr visitorAst = viaVisitor(query);
+    Expr patternAst = viaPattern(AstPrinter.toString(visitorAst));
+    return visitorAst.equals(patternAst);
+  }
+
+  @Property
+  boolean printingIsIdempotent(@ForAll("simpleQueries") String query) {
+    String once = AstPrinter.toString(viaVisitor(query));
+    String twice = AstPrinter.toString(viaVisitor(once));
+    return once.equals(twice);
+  }
+
+  @Property
+  boolean andIsCommutative(
+      @ForAll("comparisons") String a,
+      @ForAll("comparisons") String b,
+      @ForAll("mediaItems") MediaItem item) {
+    Expr ea = viaPattern(a);
+    Expr eb = viaPattern(b);
+    boolean ab = Evaluator.matches(item, new Expr.And(ea, eb));
+    boolean ba = Evaluator.matches(item, new Expr.And(eb, ea));
+    return ab == ba;
+  }
+
+  @Property
+  boolean andIsIdempotent(@ForAll("comparisons") String a, @ForAll("mediaItems") MediaItem item) {
+    Expr ea = viaPattern(a);
+    return Evaluator.matches(item, new Expr.And(ea, ea)) == Evaluator.matches(item, ea);
+  }
+
+  @Property
+  boolean andImpliesBothOperands(
+      @ForAll("comparisons") String a,
+      @ForAll("comparisons") String b,
+      @ForAll("mediaItems") MediaItem item) {
+    Expr ea = viaPattern(a);
+    Expr eb = viaPattern(b);
+    if (Evaluator.matches(item, new Expr.And(ea, eb))) {
+      return Evaluator.matches(item, ea) && Evaluator.matches(item, eb);
+    }
+    return true; // wenn das And falsch ist, ist nichts zu zeigen
+  }
 
   // ---------- @Provide-Methods for Arbitraries ----------
+
+  @Provide
+  Arbitrary<MediaItem> mediaItems() {
+    Arbitrary<String> titles = Arbitraries.of("Yesterday", "Imagine", "Bohemian", "abc", "xyz");
+    Arbitrary<String> artists = Arbitraries.of("Beatles", "Queen", "Nirvana", "abc", "xyz");
+    Arbitrary<Genre> genres = Arbitraries.of(Genre.values());
+    Arbitrary<Integer> years = Arbitraries.integers().between(1900, 2025);
+    return Combinators.combine(titles, artists, genres, years).as(MediaItem::new);
+  }
 
   @Provide
   Arbitrary<String> fields() {
